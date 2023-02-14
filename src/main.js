f83272f8371407ed4d6595212a3748f492b48cf6
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-02-14 22:25:09 +0700

    feat: create transaction page views and router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+import { createApp } from "vue";
+import router from "./router";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min";
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
